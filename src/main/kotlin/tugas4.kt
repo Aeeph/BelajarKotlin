4b0f743d113e29a3faf70cef852a6374623559b9
Aeeph <hh191099@gmail.com>
2021-09-06 16:49:41 +0700

    tugas 4

---
+++ src/main/kotlin/tugas4.kt
@@ -0,0 +1,12 @@
+fun main(args : Array<String>) {
+
+        var i = 2
+        val kota = arrayListOf("Bogor", "Jakarta", "Bekasi")
+        for (i in kota){
+            println("Kota : $i")
+        }
+    kota.forEach{
+        println("kota : $it")
+    }
+
+}
